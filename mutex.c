6e974232b5f7c5c2cc01acae8aec37e810884f3e
Pim van Leeuwen <p.vanleeuwen@student.utwente.nl>
2018-05-02 11:17:30 +0200

    Revert to Bare Bones

--- mutex.c
+++ mutex.c
@@ -1,16 +1,16 @@
-#include <sylvan.h>
 #include <stdio.h>
 #include <float.h>
+#include <sylvan.h>
 
 
 int main()
 {
-    LACE_ME;
 // 0 = auto-detect  number  of  Lace  workers
 int n_workers = 0;
 //initialize Lace with a deque size of 4M
 lace_init(n_workers, 40960000);
 lace_startup(0, NULL, NULL);
+
 
 //initialize Sylvan's node table and operations cache
 //with at least 2^20 entries, and at most 2^25 entries
@@ -21,20 +21,15 @@ sylvan_init_bdd();
 //BDD a = sylvan_and(sylvan_nithvar(1), sylvan_ithvar(2));
 
 
-
-
-int i = 0;
-char b[256];
-snprintf(b, 256, "/tmp/sylvan/BDD-%d.dot", i);
-FILE *f = fopen(b, "w+");
+//int i = 0;
+//char b[256];
+//snprintf(b, 256, "/tmp/sylvan/BDD-%d.dot", i);
+//FILE *f = fopen(b, "w+");
 //sylvan_fprintdot(f,a);
-fclose(f);
+//fclose(f);
 
-
-
-printf("Hello, World\n");
-
-
+//printf("Hello, World\n");
+    
 //if Sylvan is compiled with -DSYLVAN_STATS=ON,
 //then print statistics on stderr.
 sylvan_stats_report(stderr);
