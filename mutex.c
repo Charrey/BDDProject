04c022f80ebc73d328e389ff297c346b80c1b0a8
Pim van Leeuwen <p.vanleeuwen@student.utwente.nl>
2018-05-03 11:38:30 +0200

    Fix

--- mutex.c
+++ mutex.c
@@ -5,30 +5,34 @@
 
 int main()
 {
+
 // 0 = auto-detect  number  of  Lace  workers
 int n_workers = 0;
 //initialize Lace with a deque size of 4M
 lace_init(n_workers, 40960000);
 lace_startup(0, NULL, NULL);
-
+LACE_ME;
 
 //initialize Sylvan's node table and operations cache
 //with at least 2^20 entries, and at most 2^25 entries
 sylvan_init_package(1LL<<20,1LL<<25,1LL<<20,1LL<<25);
 sylvan_init_bdd();
 
+BDD a = sylvan_ithvar(1);
+sylvan_protect(a);
+BDD b = sylvan_nithvar(2);
+sylvan_protect(b);
+a = sylvan_and(a, b);
 
-//BDD a = sylvan_and(sylvan_nithvar(1), sylvan_ithvar(2));
 
+int i = 0;
+char c[256];
+snprintf(c, 256, "/tmp/sylvan/BDD-%d.dot", i);
+FILE *f = fopen(c, "w+");
+sylvan_fprintdot(f,a);
+fclose(f);
 
-//int i = 0;
-//char b[256];
-//snprintf(b, 256, "/tmp/sylvan/BDD-%d.dot", i);
-//FILE *f = fopen(b, "w+");
-//sylvan_fprintdot(f,a);
-//fclose(f);
-
-//printf("Hello, World\n");
+printf("Hello, World\n");
     
 //if Sylvan is compiled with -DSYLVAN_STATS=ON,
 //then print statistics on stderr.
