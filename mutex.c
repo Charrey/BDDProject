e6e64f902ab414f787da7b2f26dc81e474d8d085
Pim van Leeuwen <p.vanleeuwen@student.utwente.nl>
2018-05-07 13:37:04 +0200

    nextcommit

--- mutex.c
+++ mutex.c
@@ -2,11 +2,12 @@
 #include <float.h>
 #include <sylvan.h>
 #include <stdarg.h>
+#include <stdbool.h>
 
-uint32_t c_var = 100;
-uint32_t cp_var = 101;
-uint32_t w_var = 200;
-uint32_t wp_var = 201;
+uint32_t c_var = 200;
+uint32_t cp_var = 201;
+uint32_t w_var = 100;
+uint32_t wp_var = 101;
 uint32_t f_var = 300;
 uint32_t fp_var = 301;
 
@@ -17,9 +18,14 @@ BDD wp;
 BDD f;
 BDD fp;
 
-BDD varmap = sylvan_map_empty();
 BDD reverse_varmap = sylvan_map_empty();
 BDD varset = sylvan_set_empty();
+
+BDD initial;
+BDD transLeave;
+BDD transEnter;
+BDD transExit;
+BDD transRestart;
 
 void showBDD(BDD a) {
     int i = 0;
@@ -30,7 +36,7 @@ void showBDD(BDD a) {
     fclose(f);
 }
 
-void init() {
+void init_sylvan_lace() {
     // 0 = auto-detect  number  of  Lace  workers
     int n_workers = 0;
     //initialize Lace with a deque size of 4M
@@ -42,11 +48,6 @@ void init() {
     //with at least 2^20 entries, and at most 2^25 entries
     sylvan_init_package(1LL<<20,1LL<<25,1LL<<20,1LL<<25);
     sylvan_init_bdd();
-
-}
-
-BDD not(BDD a) {
-    return sylvan_not(a);
 }
 
 BDD and(int count, ...) {
@@ -66,24 +67,35 @@ BDD and(int count, ...) {
     return res;
 }
 
+BDD or(int count, ...) {
+    LACE_ME;
+    //initialize starting BDD
+    BDD res = sylvan_false;
+    //Declare list of variable args
+    va_list ap;
+    //Start feeding args
+    va_start(ap, count);
+    int i;
+    for(i=0; i<count; i++)
+        //feed arguments and AND them to init BDD
+        res = sylvan_or(res, va_arg(ap, BDD));
+    //End feeding args
+    va_end(ap);
+    return res;
+}
+
+BDD not(BDD a) {
+    return sylvan_not(a);
+}
+
 
 BDD is(BDD a, BDD b) {
     LACE_ME;
     return sylvan_equiv(a, b);
 }
 
-BDD next(BDD beginstate, BDD transition) {
+void init_mutex() {
     LACE_ME;
-    BDD nextstate_not_renamed = sylvan_compose(sylvan_exists(and(2, beginstate, transition), varset), reverse_varmap);
-    return nextstate_not_renamed;
-}
-
-
-int main()  {
-
-    init();
-    LACE_ME; //Waarom moet dit?
-
     //All variables as BDDs
     c = sylvan_ithvar(c_var);
     cp = sylvan_ithvar(cp_var);
@@ -103,38 +115,58 @@ int main()  {
     varset = sylvan_set_add(varset, f_var);
 
     //Initial state
-    BDD initial = and(3,not(c), w, not(f));
+    initial = and(3,not(c), w, not(f));
 
     //-------------Transitions---------------
 
     //Leave transition
-    BDD transleave = and(3, not(cp), is(w,wp), is(f,fp));
+    transLeave = and(3, not(cp), is(w,wp), is(f,fp));
 
     //Enter transition
-    BDD transEnter = and(5, not(c), w, cp, not(wp), is(f, fp));
+    transEnter = and(5, not(c), w, cp, not(wp), is(f, fp));
 
     //Exit transition
-    BDD exit = and(4, c, not(cp), not(is(f, fp)), is(w,wp));
+    transExit = and(4, c, not(cp), not(is(f, fp)), is(w,wp));
 
     //Restart transition
-    BDD restart = and(4, f, not(is(w, wp)), not(fp), is(c,cp));
+    transRestart = and(4, f, not(is(w, wp)), not(fp), is(c,cp));
 
     //--------------------------------------
+}
+
+BDD next(BDD beginstate, BDD transition) {
+    LACE_ME;
+    BDD nextstate = sylvan_compose(sylvan_exists(and(2, beginstate, transition), varset), reverse_varmap);
+    return nextstate;
+}
+
+bool check(int count, BDD toCheck, ...) {
+  //TODO HANS (and all, then check if equals sylvan_false)
+}
+
+
+int main()  {
+
+    init_sylvan_lace();
+    LACE_ME; //Waarom moet dit?
+    init_mutex();
+
+    BDD transAny = or(4, transRestart, transLeave, transEnter, transExit);
+
 
     //BDD containing all states after the transition leave from the initial state
+    //showBDD(next(initial,transLeave));
+    BDD myBDD = initial;
+    int counter = 0;
+    for (int i = 0; i < 3; ++i) {
+        myBDD = next(myBDD, transAny);
+    }
 
-    showBDD(next(initial,transleave));
+    BDD check = and(4, myBDD, not(c), not(w), f);
 
-
-
-    //BDD enter = and(not(c), and(w, and(cprime, and(not(wprime), is(f,fprime)))));
-
-
-
-
-
-
-
+    showBDD(transAny);
+    char str[12];
+    sprintf(str, "%d", sylvan_nodecount(transAny));
 
     //if Sylvan is compiled with -DSYLVAN_STATS=ON,
     //then print statistics on stderr.
